8fd7d5eedd12c128935db8407d02147576c09d36
Sophie Palmade <sophie.palmade@imt-atlantique.net>
2024-11-27 16:36:27 +0100

    Ajout key

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -5,10 +5,12 @@ import org.json4s.native.Serialization
 
 object Scalix extends App {
   println("Hello!")
-  val key = "On ne l'a pas encore"
-  val url = s"Bonne question"
+  val key = "cb7e721297b8816dfc9f1055621de1cc"
+  val url = s"https://api.themoviedb.org/3/movie/550?api_key=cb7e721297b8816dfc9f1055621de1cc"
   val source = scala.io.Source.fromURL(url)
   val contents = source.mkString
   val json = parse(contents)
   println(contents)
+
+  //def findActorId(name: String, surname: String): Option[Int] = ???
 }
